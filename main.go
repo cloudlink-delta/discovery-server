65407d63a1d57d8770d96e61c6b6df33c31a8bc1
Mike J. Renaker / "MikeDEV <mikejrenaker@proton.me>
2025-10-13 10:28:03 -0400

    Update main.go

--- main.go
+++ main.go
@@ -1,14 +1,20 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"os/signal"
 	"syscall"
 
 	"github.com/cloudlink-delta/discovery-server/server"
+	"github.com/cloudlink-delta/discovery-server/server/microkey"
 )
 
 func main() {
+	var privB64, pubB64 string
+	privB64, pubB64, _ = microkey.GetKeypair("./priv.key", "./pub.key")
+	fmt.Printf("Your private key is: %s\n", privB64)
+	fmt.Printf("Your public key is: %s\n", pubB64)
 
 	// Define a globally unique designation that will be used to identify this discovery server.
 	// A preferred format follows the following pattern: <COUNTRY>-<CITY>-<STATE/PROVINCE>-(OPTIONAL: NUMBER)
